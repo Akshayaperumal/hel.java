4ef429156dfbb8ae28e853696463657ff4ea1b1c
Akshayaperumal <akshayaperumal1996@gmail.com>
2017-06-09 11:56:55 +0530

    Create hel.java

---
+++ hel.java
@@ -0,0 +1,12 @@
+# hel.java
+import java.io.*;
+import java.util.*;
+public class hel
+{
+    public static void main(String args[])
+    {
+        String s="hello";
+        System.out.println(s+" "+s+" "+s+" "+s+" "+s);
+        
+    }
+}
